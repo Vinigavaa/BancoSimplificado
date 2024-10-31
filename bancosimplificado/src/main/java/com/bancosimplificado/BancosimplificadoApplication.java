0534dba5fc4af0db09c36fe3f2c04f753c88f4f5
Zer0nights <137710927+Zer0nights@users.noreply.github.com>
2024-10-31 14:21:24 -0300

    add Entity User and Enum UserType

---
+++ bancosimplificado/src/main/java/com/bancosimplificado/BancosimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.bancosimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancosimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancosimplificadoApplication.class, args);
+	}
+
+}
---
+++ bancosimplificado/src/test/java/com/bancosimplificado/BancosimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancosimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancosimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
